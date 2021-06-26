c57d94347bbb349a15dae5221e599bfb64e61bd8
William Sweetenham <william@sweetenham.org>
2021-06-26 16:12:02 +0100

    Added updated TestAll.cpp file and accompanying makefile

--- test/TestAll.cpp
+++ test/TestAll.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 
 #include "test/utils/TestUtils.h"
+#include "test/geometry/TestTuple.h"
 
 int main() {
     CppUnit::TextUi::TestRunner runner;
     runner.addTest(TestUtils::suite());
+    runner.addTest(TestTuples::suite());
     runner.run();
     return 0;
 }
