5aedcf6c20b5e5e910ba6589f8a4f0d4a001d23d
William Sweetenham <william@sweetenham.org>
2021-06-27 15:29:20 +0100

    Updated Material.cpp to make tests pass

---
+++ src/scene/world/object/material/Material.cpp
@@ -0,0 +1,3 @@
+#include "Material.h"
+
+Material::Material(float ambient, float diffuse, float specular, const Colour& colour): ambient(ambient), diffuse(diffuse), specular(specular), colour(colour) {}
