e780c5bce17425e7dc3b8ee11d98fe89a34835c0
William Sweetenham <william@sweetenham.org>
2021-07-02 20:22:28 +0100

    Cleaning up refactor

--- src/scene/world/object/material/Material.cpp
+++
@@ -1,3 +0,0 @@
-#include "Material.h"
-
-Material::Material(float ambient, float diffuse, float specular, const Colour& colour): ambient(ambient), diffuse(diffuse), specular(specular), colour(colour) {}
--- src/scene/world/object/material/Material.h
+++
@@ -1,9 +0,0 @@
-#include "src/basicConstructs/Colour.h"
-
-struct Material {
-    float ambient;
-    float diffuse;
-    float specular;
-    Colour colour;
-    Material(float ambient, float diffuse, float specular, const Colour& colour);
-};
