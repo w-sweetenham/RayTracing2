4cf090945bb7e5a40f707ce7843529722b28607b
William Sweetenham <william@sweetenham.org>
2021-09-19 14:16:48 +0100

    Added test to check new params appropriately added to material class

--- test/world/object/material/TestMaterial.cpp
+++ test/world/object/material/TestMaterial.cpp
@@ -11,6 +11,16 @@ void TestMaterial::testMaterial() {
     CPPUNIT_ASSERT(roughlyEqual(material.getSpecular(), 0.9));
     CPPUNIT_ASSERT(roughlyEqual(material.getShininess(), 100));
     CPPUNIT_ASSERT(material.getColour().roughlyEqual(Colour(0.2, 0.4, 0.6)));
+
+    Material material1(0.1, 0.8, 0.9, 100, Colour(0.2, 0.4, 0.6), 0.1, 0.2, 0.3);
+    CPPUNIT_ASSERT(roughlyEqual(material1.getAmbient(), 0.1));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getDiffuse(), 0.8));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getSpecular(), 0.9));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getShininess(), 100));
+    CPPUNIT_ASSERT(material1.getColour().roughlyEqual(Colour(0.2, 0.4, 0.6)));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getRefractiveIndex(), 0.1));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getReflectivity(), 0.2));
+    CPPUNIT_ASSERT(roughlyEqual(material1.getTransparency(), 0.3));
 }
 
 CppUnit::Test* TestMaterial::suite()
