9ed547da0ce0a6324efc2d48f28f6f774840c169
William Sweetenham <william@sweetenham.org>
2021-10-16 14:03:39 +0100

    Added milestone

---
+++ milestones/firstPatternedSphere/firstPatternedSphere.cpp
@@ -0,0 +1,23 @@
+#include "src/camera/Camera.h"
+#include "src/world/World.h"
+#include "src/world/lightSource/PointLight.h"
+#include "src/world/object/shape/TSphere.h"
+#include "src/world/object/shape/Cube.h"
+#include "src/world/object/material/Material.h"
+#include "src/shader/Shader.h"
+#include "src/basicConstructs/Tuple.h"
+#include "src/basicConstructs/Matrix.h"
+
+int main() {
+    Camera cam(Point(0, 1.5, -5), Vec(0, -0.5, 5), Vec(0, 1, 0), 200, 400, 0.02, 0.0001);
+    World world(PointLight(Point(-10, 10, -10), Colour(1, 1, 1)));
+    world.addObject(TSphere(TranslationMat(1.5, 1, -1.0), 4), PatternMaterial(0.2, 0.6, 0.8, 100, CheckerPattern(Colour(0, 0.2, 0.9), Colour(0.83, 0.69, 0.22)), 1.33, 0.1, 0));
+    world.addObject(Cube(TranslationMat(0, -0.1, 2)*ScalingMat(1000, 0.2, 1000), 1000), PatternMaterial(0.2, 0.8, 0.6, 100, CheckerPattern(Colour(0, 0, 0), Colour(1, 1, 1)), 1.5, 0.1, 0));
+    world.addObject(Cube(TranslationMat(0, 0, 8)*ScalingMat(1000, 1000, 0.1), 3000), PatternMaterial(0.2, 0.8, 0.6, 100, CheckerPattern(Colour(0, 0, 0), Colour(1, 1, 1)), 1.5, 0.1, 0));
+    world.addObject(TSphere(TranslationMat(-1.5, 1, -1.0)*ScalingMat(1, 1, 1)), basicMaterial(0.0, 0.0, 0.8, 100, Colour(0, 0.5, 0.8), 1.33, 0, 0.95));
+    PhongShader shader;
+    std::cout << "Setup complete" << std::endl;
+    cam.render(world, shader, 50);
+    std::cout << "Image created" << std::endl;
+    cam.save("/home/william/CPP/Projects/RayTracer2/milestones/firstPatternedSphere/firstPatternedSphere.ppm");
+}
